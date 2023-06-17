1de2a29756903fdae2bba8ff43d580bfc62f8308
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-17 13:22:43 +0000

    Add JSX to display random users

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -3,22 +3,18 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchRandomUser } from '../redux/feature/usersSlice';
 
 const UserDisplay = () => {
-  const [users] = useSelector((state) => state.users);
-  const dispatch = useDispatch();
+  const { users, isLoading, error } = useSelector((state) => state.users);
 
   return (
     <div>
       <h1>List of Users</h1>
-      <button onClick={dispatch(fetchRandomUser())}></button>
-      {
-        users.map((user, index) => (
-          <div key={index}>
-            <p>
-              {user.name.first} {user.name.last}
-            </p>
-          </div>
-        ))
-      }
+      {users.results.map((user, index) => (
+        <div key={index}>
+          <p>
+            {user.name.first} {user.name.last}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
